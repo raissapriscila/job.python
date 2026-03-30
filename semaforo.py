336e39cc18a2706e5b7c1ecc9cbf5ca38b7c753b
raissapriscila <raissapriscilsantadasilva@gmail.com>
2026-03-30 11:17:45 -0300

    Add files via upload

---
+++ semaforo.py
@@ -0,0 +1,13 @@
+import time
+
+print('semaforo vermelho')
+print('não pode passar')
+time.sleep(5)
+
+print('semaforo amarelo')
+print('atenção')
+time.sleep(3)
+
+print('semaforo verde')
+print('pode passar')
+time.sleep(5)
